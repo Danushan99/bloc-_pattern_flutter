9a69df50ecd0b105cb932d2fdbb5f2f6352ea07a
Danushan <it20051648@my.sliit.lk>
2022-03-20 14:49:45 +0530

    creating bloc pattern

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,7 @@
+import 'dart:async';
+
+class CounterBloc {
+  final stateStreamController = StreamController<int>();
+  StreamSink<int> get counterSink => stateStreamController.sink;
+  Stream<int> get counterStream => stateStreamController.stream;
+}
